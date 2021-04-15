a9de15e1251ccb57a800a73aa531369c7e04bfe9
Craig Gallimore <c.gallimore.it@gmail.com>
2021-04-15 17:41:36 +0200

    Lesson 2 code

--- COS1511/Part1/Lesson2/maths.cpp
+++ COS1511/Part1/Lesson2/maths.cpp
@@ -1,4 +1,4 @@
-// a load of math calculations and there outcomes. 
+// a load of math calculations and there outcomes.
 #include <iostream>
 
 using namespace std;
@@ -6,22 +6,31 @@ using namespace std;
 int main()
 {
   const int answer = 1 * 2 * 3 * 4 * 5;
-  cout << "The product of 1 to 5 is :" << endl << answer << endl;
+  cout << "The product of 1 to 5 is :" << endl
+       << answer << endl;
 
-  cout << "The rsult of 12 * 23 + 34 * 45 is: " << endl << (12 * 23) + (34 * 45) << endl;
+  cout << "The result of 12 * 23 + 34 * 45 is: " << endl
+       << (12 * 23) + (34 * 45) << endl;
 
-  cout << "The result of 4 times the sum of 5 and 6 is: " << endl << (5 + 6) * 4 << endl;
+  cout << "The result of 4 times the sum of 5 and 6 is: " << endl
+       << (5 + 6) * 4 << endl;
 
-  cout << "The difference between 543 and 234 is: " << endl << 543 - 234 << endl;
+  cout << "The difference between 543 and 234 is: " << endl
+       << 543 - 234 << endl;
 
-  cout << "The difference between 234 and 543 is: " << endl << 234 - 543 << endl;
+  cout << "The difference between 234 and 543 is: " << endl
+       << 234 - 543 << endl;
 
-  cout << "The quotient of 30 and 3 is: " << endl << 30 / 3 << endl;
+  cout << "The quotient of 30 and 3 is: " << endl
+       << 30 / 3 << endl;
 
-  cout << "The quotient of 20 and 3: " << endl << 20 / 3 << endl;
+  cout << "The quotient of 20 and 3: " << endl
+       << 20 / 3 << endl;
 
-  cout << "The quotient of 10 and 3: " << endl << 10 / 3 << endl;
+  cout << "The quotient of 10 and 3: " << endl
+       << 10 / 3 << endl;
 
-  cout << "The result of the sum of 357 and 468 and the product of -19 and 28: " << endl << (357 + 468) / (-19 * 28) << endl;
+  cout << "The result of the sum of 357 and 468 and the product of -19 and 28: " << endl
+       << (357 + 468) / (-19 * 28) << endl;
   return 0;
 }
